af6a2a011df3e3c3c44755b60ac4c86e49e8559f
Akshayanand Raut <akshay.raut0303@gmail.com>
2019-01-29 02:47:57 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,32 +1,12 @@
-
 const express = require('express');
 const path = require('path')
-
 const PORT = process.env.PORT || 8080
 var app = express();
-
-
-
-
-
 app
     .use(express.static(path.join(__dirname, 'public')))
-
- 
-
-
     .set('views', path.join(__dirname, 'views'))
     .set('view engine', 'ejs')
- 
-
-
-
-
 
 .get('/', function (req, res) {
-         res.render("login")
-  
-    
-              
-
-    })
+         res.render("login")  
+})
