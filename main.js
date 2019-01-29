f6fb6de5abee8d5883bb191d56db6bd8da3ea25a
Akshayanand Raut <akshay.raut0303@gmail.com>
2019-01-29 07:18:03 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,32 @@
+
+const express = require('express');
+const path = require('path')
+
+const PORT = process.env.PORT || 8080
+var app = express();
+
+
+
+
+
+app
+    .use(express.static(path.join(__dirname, 'public')))
+
+ 
+
+
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'ejs')
+ 
+
+
+
+
+
+.get('/', function (req, res) {
+         res.render("login")
+  
+    
+              
+
+    })
